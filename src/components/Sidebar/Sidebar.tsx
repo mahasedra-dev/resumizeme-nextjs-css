b1c6302855a9fbb9c1195272193e11bad0098f01
mahasedra <mahasedra01@gmail.com>
2024-02-23 15:48:03 +0300

    feat sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -5,26 +5,111 @@ const Sidebar: React.FC = () => {
     return (
         <div className="">
             <div className="sidebar-header">
-                <img src="images/logo.svg"/>
+                <img src="images/logo.svg" />
             </div>
-            <div className="sidebar-menu">
-                <p className="menu-section">My templates</p>
-                <input type="search" placeholder="Search" className="search-bar" />
-                    <ul className="menu-list">
-                        <li className="starred"><span>⭐️</span> Software Engineer</li>
-                        <li className="starred"><span>⭐️</span> Computer hardware engineer</li>
-                        <li>Past search 1</li>
-                    </ul>
-                    <p className="menu-section">My boards</p>
-                    <ul className="menu-list">
-                        <li className="board"><span>🗂️</span> Board 1</li>
-                        <li className="locked"><span>🔒</span> Board agent 1</li>
-                    </ul>
-            </div>
-            <div className="sidebar-footer">
-                <img src="carla-avatar.png" alt="Carla" className="user-avatar" />
-                    <span>Carla</span>
-                    <span className="settings-icon">⚙️</span>
+            <div className="sidebar-nav">
+                <div className="sidebar-menu-picker text-bold">
+                    <img src="images/menu-picker-template.svg" />
+                    <span>My Templates</span>
+                </div>
+                <div className="sidebar-history-container">
+                    <div className="sidebar-menu-picker ">
+                        <img src="images/search.svg" />
+                    </div>
+                    <div className="sidebar-menu-picker">
+                        <img src="images/favorite-gold.png" />
+                        <span>Software Enginner</span>
+                    </div>
+                    <div className="sidebar-menu-picker">
+                        <img src="images/favorite-gold.png" />
+                        <span>Computer hardware engineer</span>
+                    </div>
+                    <div className="sidebar-menu-picker">
+                        <img src="images/favorite-gold.png" />
+                        <span>Network Engineer</span>
+                    </div>
+                    <div className="sidebar-menu-picker">
+                        <img src="images/favorite-gold.png" />
+                        <span>Tecnical Support</span>
+                    </div>
+                    <div className="sidebar-menu-picker">
+                        <img src="images/favorite-gold.png" />
+                        <span>Network administrator</span>
+                    </div>
+                    <div className="sidebar-menu-picker">
+                        <img src="images/favorite-gold.png" />
+                        <span>Management</span>
+                    </div>
+                    <div className="sidebar-menu-picker">
+                        <img src="images/favorite-gold.png" />
+                        <span>Data analysis</span>
+                    </div>
+                    <div className="sidebar-menu-picker">
+                        <img src="images/favorite-gold.png" />
+                        <span>Computer technician</span>
+                    </div>
+                    <div className="sidebar-menu-picker-history">
+                        <img src="images/clock.svg" />
+                        <span>Post search 1</span>
+                    </div>
+                    <div className="sidebar-menu-picker-history">
+                        <img src="images/clock.svg" />
+                        <span>Post search 2</span>
+                    </div>
+                    <div className="sidebar-menu-picker-history">
+                        <img src="images/clock.svg" />
+                        <span>Computers and information...</span>
+                    </div>
+                    <div className="sidebar-menu-picker-history">
+                        <img src="images/clock.svg" />
+                        <span>Database Administrator</span>
+                    </div>
+                    <div className="sidebar-menu-picker-history">
+                        <img src="images/clock.svg" />
+                        <span>Computer security</span>
+                    </div>
+                    <div className="sidebar-menu-picker-history">
+                        <img src="images/clock.svg" />
+                        <span>Computer Systems Analyst</span>
+                    </div>
+                </div>
+                <div className="sidebar-board-container">
+                    <div style={{display: "flex", alignItems:"center", justifyContent:"space-between", padding: "6px 4px"}}>
+                        <div className="sidebar-menu-picker text-bold">
+                            <img src="images/menu-picker-board.svg" />
+                            <span>My boards</span>
+                        </div>
+                        <div>
+                            <img src="images/mini-add.svg" />
+                        </div>
+                    </div>
+                    <div className="sidebar-board-container">
+                        <div className="sidebar-menu-picker">
+                            <img src="images/board-file-1.png" />
+                            <span>Board 1</span>
+                        </div>
+                        <div className="sidebar-menu-picker">
+                            <img src="images/board-file-1.png" />
+                            <span>Board 2</span>
+                        </div>
+                        <div className="sidebar-menu-picker">
+                            <img src="images/board-file-1.png" />
+                            <span>Board 3</span>
+                        </div>
+                        <div className="sidebar-menu-picker">
+                            <img src="images/board-file-2.png" />
+                            <span>Board agent 1</span>
+                        </div>
+                        <div className="sidebar-menu-picker">
+                            <img src="images/board-file-2.png" />
+                            <span>Board agent 2</span>
+                        </div>
+                        <div className="sidebar-menu-picker">
+                            <img src="images/board-file-2.png" />
+                            <span>Board agent 3</span>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
